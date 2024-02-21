4b36e155cbefd6803322b9fdf0b4e908baf3c908
SyedAbrarTirmizi <sabrar500@gmail.com>
2024-02-21 16:10:28 -0600

    practicing Console

---
+++ app.js
@@ -0,0 +1,13 @@
+// Variables
+let practice = "JavaScript";
+
+console.log(practice);
+
+let age = "25";
+
+console.log(age + 5)
+console.log(25 + 5)
+console.log("25" + 5)
+console.log("25 + 5")
+console.log("age + 5")
+console.log(age + "5")
