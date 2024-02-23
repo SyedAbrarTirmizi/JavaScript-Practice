57b8e9b845015fed1d1bc9dcd94d5d28636ceae3
SyedAbrarTirmizi <sabrar500@gmail.com>
2024-02-22 19:05:11 -0600

    Variables of JavaScript

--- prompt/script.js
+++ prompt/script.js
@@ -1,16 +1,16 @@
-// // agar variable ma kuxh store nhi hoga tw undefined store hoga
-// let otherName; // undefined
+// agar variable ma kuxh store nhi hoga tw undefined store hoga
+let otherName; // undefined
 
-// let myName = "Shehzad";
+let myName = "Shehzad";
 
-// console.log("my name is ", myName);
+console.log("my name is ", myName);
 
-// // changing variable value
-// myName = "Saylani";
+// changing variable value
+myName = "Saylani";
 
-// console.log(myName);
+console.log(myName);
 
-// myName = "testing";
+myName = "testing";
 
 
 
@@ -20,27 +20,27 @@ let userName = prompt("what is your name");
 alert("Welcome " + userName);
 
 
-console.log(userName);
+
 
 
 // Prompt se hum user se input lete hyn
-// let num1 = prompt("enter first number");
-// let num2 = prompt("enter second number");
+let num1 = prompt("enter first number");
+let num2 = prompt("enter second number");
 
-// // string ko number ma convert kiya hy
-// num1 = Number(num1);
-// num2 = Number(num2);
+// string ko number ma convert kiya hy
+num1 = Number(num1);
+num2 = Number(num2);
 
-// console.log("num1: ", num1);
-// console.log("num2: ", num2);
+console.log("num1: ", num1);
+console.log("num2: ", num2);
 
-// let sum = num1 + num2;
-// let minus = num1 - num2;
+let sum = num1 + num2;
+let minus = num1 - num2;
 
-// alert("Addition " + sum);
-// alert("Subtraction " + minus);
-// alert("Multiplication " + num1 * num2);
-// alert("Division " + num1 / num2);
+alert("Addition " + sum);
+alert("Subtraction " + minus);
+alert("Multiplication " + num1 * num2);
+alert("Division " + num1 / num2);
 
 // In programming a sentence is a statement
 // programming ka fullstop hy semicolon;
@@ -55,7 +55,7 @@ comment
 
 */
 
-// // const ko change nhi kar sagty ap
-// const email = "shehzad@gmail.com";
+// const ko change nhi kar sagty ap
+const email = "shehzad@gmail.com";
 
-// email = "something"; // error
+email = "something"; // error
