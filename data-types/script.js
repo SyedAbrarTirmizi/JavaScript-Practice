59240635dcd6fddb56751881f336b6e09639c419
SyedAbrarTirmizi <sabrar500@gmail.com>
2024-02-27 18:12:36 -0600

    JavaScript data-types practice

---
+++ data-types/script.js
@@ -0,0 +1,25 @@
+let myName = `Shehzad`; // String
+let age = 50; // Number
+
+// Boolean
+let lightSwitch = false;
+let lightSwitch2 = true;
+
+// undefined
+let test = undefined;
+let test1;
+
+// null
+let empty = null;
+
+// symbol
+// bigint
+
+// Primitive data types are 7
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. undefined
+// 5. null
+// 6. BigInt
+// 7. Symbol
